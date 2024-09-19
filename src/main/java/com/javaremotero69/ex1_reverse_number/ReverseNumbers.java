c5071802ea903547ec1c8e982900ebecfb6f5729
Iancu Jianu <iancujianu98@gmail.com>
2024-09-19 19:36:16 +0300

    init ex1

---
+++ src/main/java/com/javaremotero69/ex1_reverse_number/ReverseNumbers.java
@@ -0,0 +1,32 @@
+package com.javaremotero69.ex1_reverse_number;
+
+/*
+    Reverse a number using while Loop.
+    The program will prompt user to input the number,
+    and then it will reverse the same number using while loop.
+
+    8765 -> 5678
+
+    Metoda 1: 8765 = 8000+700+60+5=5 cu restul impartirii la 10
+     while(???) {
+
+    }
+
+    Metoda 2:
+    8765 -> array?
+    array = {8, 7, 6, 5}
+    parcurs invers
+
+    Metoda 3:
+    8765 -> toString -> List<Character>
+
+
+    Ce e complexitatea? De ce alegem varianta 1 in locul lui 2 si 3?
+*/
+
+public class ReverseNumbers {
+
+    public static void main(String[] args) {
+        // Scanner - citire
+    }
+}

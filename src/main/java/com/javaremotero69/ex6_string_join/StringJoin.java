f4a44ba984332a4b995b8e2baec3427c961455e2
Iancu Jianu <iancujianu98@gmail.com>
2024-09-24 20:58:28 +0300

    final ex6

---
+++ src/main/java/com/javaremotero69/ex6_string_join/StringJoin.java
@@ -0,0 +1,73 @@
+package com.javaremotero69.ex6_string_join;
+
+/*
+    Write a method that returns a comma-separated string based on a given list of integers.
+    Each element should be preceded by the letter 'e' if the number is even, and preceded by
+    the letter 'o' if the number is odd. For example, if the input list is (3,44), the output
+    should be 'o3,e44'.
+
+    Req:
+        - elements preceded by the letter 'e' if the number is even (numar par)
+        - elements preceded by the letter 'o' if the number is odd (numar impar)
+        - final output is a concatenation
+
+    Input:  List<Integer> = {3, 44, 2, 5}
+    Output:
+        Stadiul intermediar -> List<String> = "o3", "e44", "e2", "o5"
+        Stadiul final       -> String = "o3,e44,e2,o5"
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringJoin {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(3, 44, 2, 5));
+        System.out.println("Metoda prin for: " + methodFor(numbers));
+        System.out.println("Metoda prin for: " + methodFor2(numbers));
+        System.out.println("Metoda prin stream: " + methodStreams(numbers));
+    }
+
+    private static String methodFor2(List<Integer> numbers) {
+        List<String> result = new ArrayList<>();
+
+        for (Integer it : numbers) {
+            result.add(convertIntToStrForm(it));
+        }
+
+        return String.join(",", result);    // join preia elementele si pune "," intre ele
+    }
+
+    private static String methodStreams(List<Integer> numbers) {
+        return numbers
+                .stream()
+                .map(it -> convertIntToStrForm(it))
+                .collect(Collectors.joining(","));
+    }
+
+    private static String convertIntToStrForm(Integer value) {
+        if (value % 2 == 0) {
+            return "e" + value;
+        } else {
+            return "o" + value;
+        }
+    }
+
+    private static List<String> methodFor(List<Integer> numbers) {
+        List<String> numbersToString2 = new ArrayList<>();
+
+        for (Integer currentNumbers : numbers) {
+            if (currentNumbers % 2 == 0) {
+                String par = "e" + currentNumbers;
+                numbersToString2.add(par);
+            } else {
+                String impar = "o" + currentNumbers;
+                numbersToString2.add(impar);
+            }
+
+        }
+        return numbersToString2;
+    }
+}
